a2da113408981f80ca0a6f2c1b3fc88634590083
phamtanphat <phatdroid94@gmail.com>
2022-02-26 20:57:44 +0700

    getter setter

--- animal.dart
+++ animal.dart
@@ -1,5 +1,28 @@
-class Animal{
-  late String name;
-  late double weight;
+class Animal {
+  late String _name;
+  late double _weight;
 
+  // phương thức khởi tạo
+  //primary constructor
+  Animal({required String name, required double weight}) {
+    _name = name;
+    _weight = weight;
+  }
+
+  // second .. constructor
+  Animal.create2({required String name}) {
+    _name = name;
+  }
+
+  String get name => _name;
+
+  set name(String name) => _name = name;
+
+  double get weight {
+    return _weight;
+  }
+
+  set weight(double weight) {
+    _weight = weight;
+  }
 }
